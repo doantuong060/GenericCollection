fffbaf204a8a9f46a67c1571f059b5bdf8ad7925
doantuong060 <doantuong060@gmail.com>
2026-03-17 23:48:28 +0700

    Bai16 - Dem so lan xuat hien

---
+++ Bai16/Bai16/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ho va Ten: Doan Ngoc Tuong_2415053122348");
+            Console.WriteLine("\n\n");
+
+            Console.Write("Nhap chuoi: ");
+            string chuoi = Console.ReadLine();
+
+            Dictionary<char, int> dem = new Dictionary<char, int>();
+
+            foreach (char c in chuoi)
+            {
+                if (dem.ContainsKey(c))
+                {
+                    dem[c]++;
+                }
+                else
+                {
+                    dem[c] = 1;
+                }
+            }
+
+            Console.WriteLine("Ket qua:");
+            foreach (var i in dem)
+            {
+                Console.Write(i.Key + ": " + i.Value + " ");
+            }
+        }
+    }
+}
