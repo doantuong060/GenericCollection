bfa9eebfeaa9b421ec30100759b500904b984d20
doantuong060 <doantuong060@gmail.com>
2026-03-17 23:47:25 +0700

    Bai15 - Su dung Dictionary

---
+++ Bai15/Bai15/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bai15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ho va Ten: Doan Ngoc Tuong_2415053122348");
+            Console.WriteLine("\n\n");
+
+            Dictionary<string, string> tudien = new Dictionary<string, string>();
+
+            tudien.Add("hello", "xin chao");
+            tudien.Add("book", "quyen sach");
+            tudien.Add("computer", "may tinh");
+            tudien.Add("student", "sinh vien");
+
+            Console.Write("Nhap tu tieng Anh can tra: ");
+            string tu = Console.ReadLine().ToLower();
+
+            if (tudien.ContainsKey(tu))
+            {
+                Console.WriteLine($"Nghia tieng Viet: {tudien[tu]}");
+            }
+            else
+            {
+                Console.WriteLine("Khong tim thay tu nay!");
+            }
+        }
+    }
+}
