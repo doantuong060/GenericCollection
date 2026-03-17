c8c287ef2dc448cc13e61fda2191fd8a1efcea5d
doantuong060 <doantuong060@gmail.com>
2026-03-17 23:13:09 +0700

    Bai2 - Tinh tong cac phan tu

---
+++ Bai2/Bai2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ho va Ten: Doan Ngoc Tuong_2415053122348");
+            Console.WriteLine("\n\n");
+
+            List<int> dssonguyen = new List<int>();
+
+            int n, x;
+            Console.Write("Nhap so luong phan tu: ");
+            int.TryParse(Console.ReadLine(), out n);
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Nhap phan tu thu {0}: ", i + 1);
+                int.TryParse(Console.ReadLine(), out x);
+                dssonguyen.Add(x);
+            }
+            Console.WriteLine("\n");
+            int tg = 0;
+            foreach (int j in dssonguyen)
+            {
+                tg = tg + j;
+            }
+            Console.WriteLine("Tong cac phan tu trong danh sach la: {0}", tg);
+
+        }
+    }
+}
