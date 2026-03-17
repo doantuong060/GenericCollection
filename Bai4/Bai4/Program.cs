c03142b2cc71e57ad267658c7c4c82fd27939976
doantuong060 <doantuong060@gmail.com>
2026-03-17 23:19:37 +0700

    Bai4 - Dem so chan

---
+++ Bai4/Bai4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ho va Ten: Doan Ngoc Tuong_2415053122348");
+            Console.WriteLine("\n\n");
+
+            List<int> dssonguyen = new List<int>();
+
+            Console.Write("Nhap so luong phan tu: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Nhap phan tu thu {0}: ", i + 1);
+                int.TryParse(Console.ReadLine(), out int x);
+                dssonguyen.Add(x);
+            }
+            Console.WriteLine("\n");
+
+            int dem = 0;
+            foreach (int i in dssonguyen) 
+            {
+                if (i % 2 == 0)
+                {
+                    dem++;
+                }
+            }
+
+            Console.WriteLine("So luong so chan co trong danh sach la: {0}", dem);
+        }
+    }
+}
