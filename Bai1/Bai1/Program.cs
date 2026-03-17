5fd55341f8ac721c67633e09318cce5704861ba6
doantuong060 <doantuong060@gmail.com>
2026-03-17 22:39:59 +0700

    Bai1 - Tao danh sach sinh vien

---
+++ Bai1/Bai1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ho va Ten: Doan Ngoc Tuong_2415053122348");
+            Console.WriteLine("\n\n");
+
+            List<int> dssonguyen = new List<int>();
+
+            dssonguyen.Add(12);
+            dssonguyen.Add(5);
+            dssonguyen.Add(67);
+            dssonguyen.Add(27);
+            dssonguyen.Add(8);
+
+            Console.WriteLine("Danh sach cac phan tu trong danh sach: ");
+            foreach (int i in dssonguyen)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
